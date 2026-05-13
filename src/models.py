2f21c8023269c5f687cba304d7b63f1ada798230
Tei <jmsarmiento0304@gmail.com>
2026-05-13 12:20:53 +0800

    commited

--- src/models.py
+++ src/models.py
@@ -2,6 +2,33 @@ from sqlalchemy import Column, Integer, String, Boolean, Enum, ForeignKey, DECIM
 from sqlalchemy.orm import relationship
 from src.database import Base
 
+
+#Independent Table
+    #Users (Done)
+    #Location
+    #Amenities
+
+#1st Level Dependent Table
+    #Boarding House
+    #Photo (Done)
+    #Admin Logs
+    #Reports
+    #Notification
+
+#2nd Level Dependent Table
+    #Rooms
+    #Listing Amenities
+    #Favorites
+
+#3rd Level Dependent Table
+    #Bookings
+
+#4th Level Dependent Table
+    #Reviews
+    #Booking History
+    #Payment
+
+
 class Users(Base):
     __tablename__ = "USERS"
 
@@ -16,6 +43,33 @@ class Users(Base):
     id_document_url = Column(String(255))
     is_verified = Column(Boolean, default=False)
     status = Column(Enum('active', 'banned', 'suspended'), default='active')
+
+class Location(Base):
+    __tablename__ = "LOCATION"
+
+    location_id = Column(Integer, primary_key=True, autoincrement=True)
+    street = Column(String(255))
+    barangay = Column(String(100))
+    city = Column(String(100))
+    province = Column(String(100))
+    latitude = Column(DECIMAL(10, 8))
+    longtitude = Column(DECIMAL(11, 8))
+
+class Boarding_House(Base):
+    __tablename__ = "BOARDING_HOUSE"
+
+    listing_id = Column(Integer, primary_key=True, autoincrement=True)
+    owner_id = Column(Integer, ForeignKey("USERS.user_id", ondelete="CASCADE"), nullable=False)
+    location_id = Column(Integer, ForeignKey("LOCATION.location_id", ondelete="CASCADE"), nullable=False)
+    bh_name = Column(String(255), nullable=False)
+    description = Column(Text)
+    price_range = Column(String(100))
+    status = Column(Enum('active', 'pending', 'banned'), default='pending')
+    bh_created_at = Column(DateTime, server_default=func.now())
+    is_verified = Column(Boolean, default=False)
+    permit_url = Column(String(255))
+    min_stay_months = Column(Integer, default=1)
+    rules = Column(Text)
 
 class Photo(Base):
     __tablename__ = "PHOTOS"
