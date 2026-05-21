9ad2c21a36a79742189a2ecf5ebe245f236232c5
Tei <jmsarmiento0304@gmail.com>
2026-05-21 22:34:46 +0800

    Refactor user creation logic and improve error handling in users router

--- src/routers/users.py
+++ src/routers/users.py
@@ -2,28 +2,34 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from src import crud, schemas, database
 
+# 1. CREATE ROUTER
+# Kapag sa ibang table, palitan niyo lang yung prefix at tags (ex: "/boardinghouse")
 router = APIRouter(prefix="/users", tags=['Users'])
 
+# 2. POST ROUTE (CREATE)
+# Gagamitin ito para mag-save ng bagong user sa database.
 @router.post("/", response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
-    
-    user_crud = crud.UserCRUD
+    # call ang "engine" natin sa crud.py
+    user_crud = crud.UserCRUD(db)
 
+    # Error Check: Kapag existing na yung email, mag-rereject tayo (400) since may unique identifier ung email sa models.py
+    # Kayo na bahala kung anong possible error sa isang class
     if user_crud.get_user_by_email(user.email):
         raise HTTPException(status_code=400, detail="Email already registered")
     
-    return user_crud.create(
-        name=user.name,
-        email=user.email,
-        password=user.password,
-        role=user.role
-    )
+    # Save sa database. 
+    return user_crud.create(**user.model_dump())
 
+# 3. GET ROUTE (READ)
+# Gagamitin ito para kumuha ng specific user gamit ang ID nila sa URL.
 @router.get("/{user_id}", response_model=schemas.UserResponse)
 def read_user(user_id: int, db: Session = Depends(database.get_db)):
+    # call ang .get() function sa crud.py para hanapin yung user sa DB
     user = crud.UserCRUD(db).get(user_id)
+    
+    # Error Check: Kapag walang nahanap na user sa database (None), mag-rereject tayo (404)
     if not user:
         raise HTTPException(status_code=404, detail="User Not Found")
+        
     return user
-
-#edit text
