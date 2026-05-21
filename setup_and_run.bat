53a8e7b0a344bbfe0144fb3047ab14d4f0607b0e
Tei <jmsarmiento0304@gmail.com>
2026-05-21 16:57:32 +0800

    commited

--- setup_and_run.bat
+++ setup_and_run.bat
@@ -3,8 +3,22 @@ if not exist .venv (
     echo Creating virtual environment...
     python -m venv .venv
     call .venv\Scripts\activate
+    
     echo Installing dependencies...
-    python -m pip install -r requirements.txt
+    python -m pip install -r requirements.txt || (
+        echo.
+        echo ------------------------------------------
+        echo ERROR: Installation failed!
+        echo ------------------------------------------
+        pause
+        exit /b 1
+    )
+    
+    echo.
+    echo ------------------------------------------
+    echo Dependencies installed successfully!
+    echo ------------------------------------------
+    pause
 ) else (
     call .venv\Scripts\activate
 )
